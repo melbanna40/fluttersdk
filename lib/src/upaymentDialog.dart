a533beaee236448137ff4c52d5091ee23ecc2c90
MohamedElBanna <mel.banna40@gmail.com>
2024-01-30 22:42:10 +0200

    update packages

--- lib/src/upaymentDialog.dart
+++ lib/src/upaymentDialog.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_upayments/src/u_data.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_plus/webview_flutter_plus.dart';
 
 class upayDialog extends StatefulWidget {
-    paymentDetails data;
-    String? weblink;
-    Function(
-      bool status,
-      Map TransactionDetails,
-      String message,
-    )
-        OnSuccess;
-    Function(bool status, Map TransactionDetails,String message,) OnFailure;
-  upayDialog({Key ?key,required this.data,required this.OnFailure,required this.OnSuccess,required this.weblink }) : super(key: key);
+  paymentDetails data;
+  String? weblink;
+  Function(
+    bool status,
+    Map TransactionDetails,
+    String message,
+  ) OnSuccess;
+  Function(
+    bool status,
+    Map TransactionDetails,
+    String message,
+  ) OnFailure;
+
+  upayDialog(
+      {Key? key,
+      required this.data,
+      required this.OnFailure,
+      required this.OnSuccess,
+      required this.weblink})
+      : super(key: key);
 
   @override
   _upayDialogState createState() => _upayDialogState();
 }
 
 class _upayDialogState extends State<upayDialog> {
-  
-    NavigationDecision _interceptNavigation(NavigationRequest request) {
+  NavigationDecision _interceptNavigation(NavigationRequest request) {
     print(request.url);
 
-
-    if(request.url.contains('https://example.com/error.html')){
-      var data= Uri.dataFromString(request.url);
+    if (request.url.contains('https://example.com/error.html')) {
+      var data = Uri.dataFromString(request.url);
 // print(data.queryParametersAll);
-Map val =data.queryParametersAll;
-          Fluttertoast.showToast(
+      Map val = data.queryParametersAll;
+      Fluttertoast.showToast(
               msg: "Something Went Wrong!\nTry Again Later",
               toastLength: Toast.LENGTH_LONG,
               gravity: ToastGravity.SNACKBAR,
@@ -37,15 +45,15 @@ Map val =data.queryParametersAll;
               backgroundColor: Colors.black,
               textColor: Colors.white,
               fontSize: 16.0)
-          .then((value) =>  widget.OnFailure(false,val,"Payment Failed"));
-Navigator.pop(context);
+          .then((value) => widget.OnFailure(false, val, "Payment Failed"));
+      Navigator.pop(context);
       return NavigationDecision.prevent;
     }
-    if(request.url.startsWith('https://example.com/success.html')){
-var data= Uri.dataFromString(request.url);
+    if (request.url.startsWith('https://example.com/success.html')) {
+      var data = Uri.dataFromString(request.url);
 // print(data.queryParametersAll);
-Map val =data.queryParametersAll;
-         Fluttertoast.showToast(
+      Map val = data.queryParametersAll;
+      Fluttertoast.showToast(
               msg: "Payment Completed Successfully",
               toastLength: Toast.LENGTH_LONG,
               gravity: ToastGravity.SNACKBAR,
@@ -53,45 +61,44 @@ Map val =data.queryParametersAll;
               backgroundColor: Colors.black,
               textColor: Colors.white,
               fontSize: 16.0)
-          .then((value) =>  widget.OnSuccess(false,val,'Payment succeed'));
-Navigator.pop(context);
+          .then((value) => widget.OnSuccess(false, val, 'Payment succeed'));
+      Navigator.pop(context);
 
       return NavigationDecision.prevent;
     }
     return NavigationDecision.navigate;
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return  Dialog(
+    return Dialog(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
       ),
       elevation: 0,
-      insetPadding:EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0) ,
+      insetPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
       backgroundColor: Colors.transparent,
       child: WillPopScope(
-        onWillPop: () async{
+        onWillPop: () async {
           return false;
         },
-        child:  Stack(
-                  children: [
-                 ClipRRect(
-          borderRadius: BorderRadius.all(
-            const Radius.circular(20.0),
-          ),
-                      child: WebView(
-                        initialUrl:widget.weblink,
-                        javascriptMode: JavascriptMode.unrestricted,
-                        navigationDelegate:  _interceptNavigation,
-                      ),
-                    ),
-                 
-                  ],
-                ),
-    ),);
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.all(
+                const Radius.circular(20.0),
+              ),
+              child: WebViewWidget(
+                controller: WebViewControllerPlus()
+                  ..setNavigationDelegate(NavigationDelegate(
+                      onNavigationRequest: _interceptNavigation))
+                  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+                  ..loadRequest(Uri.parse(widget.weblink!)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
-
- 
